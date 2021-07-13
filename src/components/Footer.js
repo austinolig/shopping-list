385d373916026472b7e040ed6bb93c6f78c40d87
Austin Oligario <austinoligario@gmail.com>
2021-07-13 19:30:09 -0400

    updated text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
       {/* <a href="/about"> */}
       <button className="btn btnAbout">About</button>
       {/* </a> */}
-      <p>&copy; 2021 Austin Oligario.</p>
+      <p>&copy; 2021 Austin Oligario</p>
     </footer>
   );
 };
