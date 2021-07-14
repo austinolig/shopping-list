c74d2d36d0b9ba93bc62d9527e4d240f464785e8
Austin Oligario <austinoligario@gmail.com>
2021-07-14 11:17:22 -0400

    added state variables for editing item details

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,48 @@
 import { FiXSquare, FiEdit3 } from "react-icons/fi";
+import { useState } from "react";
 
 const Item = ({ item, onDelete, onUpdate }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [itemName, setItemName] = useState(item.name);
+  const [itemQuantity, setItemQuantity] = useState(item.quantity);
+  // const [itemImportant, setItemImportant] = useState(item.important);
+
   return (
     <tr className="item" style={{ fontWeight: item.important && "bold" }}>
-      <td>{item.quantity}</td>
-      <td>{item.name}</td>
+      <td>
+        {isEditing ? (
+          <input
+            type="number"
+            value={itemQuantity}
+            onChange={(e) => {
+              console.log(`New value = ${e.target.value}`);
+              setItemQuantity(e.target.value);
+            }}
+          ></input>
+        ) : (
+          itemQuantity
+        )}
+      </td>
+      <td>
+        {isEditing ? (
+          <input
+            type="text"
+            value={itemName}
+            onChange={(e) => {
+              console.log(`New value = ${e.target.value}`);
+              setItemName(e.target.value);
+            }}
+          ></input>
+        ) : (
+          itemName
+        )}
+      </td>
       <td>
         <FiEdit3
           onClick={() => {
-            onUpdate(item.id, item);
+            setIsEditing(!isEditing);
+            // onUpdate(item.id, item);
+            //ON CLICK CHANGE TO SAVE ICON OR SUM
           }}
           style={{ color: "grey" }}
         />
